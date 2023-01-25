9b81c9686dfe74303df8e223ffede7a34d7ef663
Josh <zylo.codes@gmail.com>
2023-01-25 15:09:18 -0600

    Toggle saved repos

--- src/components/listItem.js
+++ src/components/listItem.js
@@ -1,19 +1,36 @@
-const ListItem = ({ description, full_name, html_url, stargazers_count }) => {
+import Button from './button'
+
+const ListItem = ({ repo, saved = false, toggleSaved }) => {
+  const { description, full_name, html_url, stargazers_count, owner } = repo
+  
+  const btnClass = saved ? 'btn-success' : 'btn-outline-light'
+
+  const icon = saved 
+    ? <i className="bi bi-bookmark-check-fill"></i> 
+    : <i className="bi bi-bookmark"></i> 
+
   return (
-    <li class="list-group-item d-flex justify-content-between align-items-start bg-dark text-light">
-      <div class="ms-2 me-auto">
-        <div class="fw-bold">
+    <li className="list-group-item d-flex justify-content-between align-items-start bg-dark text-light">
+      <img src={owner.avatar_url} style={{ width: 75, height: 'auto' }} />
+      <div className="ms-2 me-auto">
+        <div className="fw-bold">
           <a href={html_url} target="_blank">
-            {full_name}
+            {full_name} <i className="bi bi-box-arrow-up-right"></i>
           </a>
         </div>
-        {description}
-      </div>
-      <div>
-        <span class="badge bg-primary rounded-pill">
-          <i class="bi bi-star-fill"></i> {stargazers_count}
+        {description}<br/>
+        <span className="badge bg-primary rounded-pill">
+          <i className="bi bi-star-fill"></i> {stargazers_count}
         </span>
       </div>
+
+      <Button 
+        onClick={() => toggleSaved(repo)}
+        className={btnClass}
+      >
+        {icon}
+      </Button>
+      
     </li>
   )
 }
--- src/pages/search.js
+++ src/pages/search.js
@@ -1,21 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Container from '../components/container'
 import SearchForm from '../components/searchForm'
 import Spinner from '../components/spinner'
 import List from '../components/list'
 import ListItem from '../components/listItem'
 import API from '../utils/api'
+import { lsKey } from '../utils/constants'
 
 const SearchPage = () => {
   const [loading, setLoading] = useState(false)
-  const [term, setTerm] = useState('')
+  const [term, setTerm] = useState('react')
   const [repos, setRepos] = useState([])
+  const [savedRepos, setSavedRepos] = useState([])
+  
+  useEffect(() => {
+    loadSavedRepos()
+  }, [])
+
+  const loadSavedRepos = () => {
+    const lsRepos = JSON.parse(localStorage.getItem(lsKey)) || []
+    setSavedRepos(lsRepos)
+  }
 
   const searchRepos = async () => {
     if (term) {
       try {
         setLoading(true)
-        const response = await API.searchRepos(term)
+        const response = await API.searchRepos(term)        
         setRepos(response.data.items)
       } catch(err) {
         console.log(err)
@@ -25,6 +36,21 @@ const SearchPage = () => {
     }
   }
 
+  const toggleSaved = repo => {
+    const foundRepo = savedRepos.find(savedRepo => savedRepo.id === repo.id)
+    let updatedSavedRepos
+    // if is in saved array
+    if (foundRepo) {
+      // remove repo from list
+      updatedSavedRepos = savedRepos.filter(savedRepo => savedRepo.id !== repo.id)
+    } else {
+      // add repo to list
+      updatedSavedRepos = [...savedRepos, repo]
+    }
+    localStorage.setItem(lsKey, JSON.stringify(updatedSavedRepos))
+    loadSavedRepos()
+  }
+  
   return (
     <Container>
       <h1 className='mt-3'>Search Github Repos: {term}</h1>
@@ -36,7 +62,17 @@ const SearchPage = () => {
       {loading 
         ? <Spinner />
         : <List>
-            {repos.map(repo => <ListItem {...repo} key={repo.id} />)}
+            {repos.map(repo => {
+              const saved = savedRepos.find(savedRepo => savedRepo.id === repo.id)
+              return (
+                <ListItem 
+                  repo={repo} 
+                  saved={saved}
+                  toggleSaved={toggleSaved}
+                  key={repo.html_url} 
+                />
+              )
+            })}
           </List>
       }
     </Container>
